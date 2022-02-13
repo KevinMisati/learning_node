422baee2ebd4cbed407da267e4b4f0240f2b043b
kevinmisati <kevinmosigisi2001@gmail.com>
2022-02-13 15:24:34 +0300

    static styles

--- max-udemy/expreess/app.js
+++ max-udemy/expreess/app.js
@@ -1,21 +1,24 @@
-const express = require("express")
-const path = require("path")
+const path = require('path');
 
-const adminRoutes = require("./routes/admin")
-const shopRoutes = require("./routes/shop")
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const bodyParser = require('body-parser')
+const app = express();
 
-const app  = express()
+app.set("view engine","pug")
+app.set("views","views")
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname,'public')))
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminRoutes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next) => {
-    res.status(404)
-    res.sendFile(path.join(__dirname, "views","404.html"))
-})
-app.listen(3000)
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render("404")
+});
+
+app.listen(3000);
--- max-udemy/expreess/routes/shop.js
+++ max-udemy/expreess/routes/shop.js
@@ -1,9 +1,15 @@
-const express = require("express")
-const path = require("path")
-const rootDir = require('../util/path')
+const path = require('path');
 
-const router = express.Router()
-router.get("/", (req, resp, next) => {
-    resp.sendFile(path.join(rootDir,"views","shop.html"))
-})
-module.exports = router
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require("./admin")
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products
+  res.render('shop',{prods:products,docTitle:"Shop"})
+});
+
+module.exports = router;
